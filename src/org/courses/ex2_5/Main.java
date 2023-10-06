4e9911117536090cc643ac16de4a56335b231c1c
KaterynaMykytko <ekaterina.kaliazina@gmail.com>
2023-10-07 00:30:56 +0200

    Add new project

---
+++ src/org/courses/ex2_5/Main.java
@@ -0,0 +1,11 @@
+package org.courses.ex2_5;
+
+public class Main {
+    public static void main(String[] args) {
+
+        float radius = 7.5f;
+
+        float circleArea = MyArea.calculateCircleArea(7.5f);
+        System.out.println("Circle area = " + circleArea);
+    }
+}
---
+++ src/org/courses/ex2_5/MyArea.java
@@ -0,0 +1,11 @@
+package org.courses.ex2_5;
+
+public class MyArea {
+
+    public static final float PI = 3.14f;
+    int radius;
+
+    public static float calculateCircleArea(float radius){
+        return 2* PI * radius;
+    }
+}
